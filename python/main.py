dfa063bd05c5d47e03ecc7df7b70a54b4634e8e6
JonathanBrouwer <j.t.brouwer@student.tudelft.nl>
2020-02-03 16:55:15 +0100

    Restructure pt 2

--- python/main.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-import increment
-
-print(incrementer_one(2))
-print(Incrementer(5).increment_by(3))
--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,13 @@
 use pyo3::prelude::*;
-use pyo3::wrap_pyfunction;
 
-#[pyclass(module = "increment")]
-struct Incrementer {
-    by: usize
-}
+#[pyclass]
+struct DummyClass {}
 
 #[pymethods]
-impl Incrementer {
-    #[new]
-    fn new(by: usize) -> Self {
-        Incrementer { by }
-    }
-
-    fn increment_by(&self, inp: usize) -> PyResult<usize> {
-        Ok(inp + self.by)
+impl DummyClass {
+    #[staticmethod]
+    fn get_42() -> PyResult<usize> {
+        Ok(42)
     }
 }
 
@@ -24,24 +17,8 @@ fn incrementer_one(inp: usize) -> usize {
 }
 
 #[pymodule]
-fn increment(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
-    m.add_wrapped(wrap_pyfunction!(incrementer_one))?;
-    m.add_class::<Incrementer>()?;
+fn increment(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<DummyClass>()?;
 
     Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::{incrementer_one, Incrementer};
-
-    #[test]
-    fn test_increment_one() {
-        assert_eq!(4, incrementer_one(3));
-    }
-
-    #[test]
-    fn test_incrementer() {
-        assert_eq!(7, Incrementer::new(5).increment_by(2).unwrap());
-    }
 }
